d3bd971e3e06f1fe5e98116e32938ff07b6d9f73
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-05-13 17:55:41 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    console.log(localStorage);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+console.log(parsedTime);
+
+const { seconds } = parsedTime;
+console.log(seconds);
+
+player.setCurrentTime(seconds);

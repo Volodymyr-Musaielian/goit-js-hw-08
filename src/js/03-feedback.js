ca403a0392e5bc9f667020e8b94728da54ae0ff4
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-05-14 23:01:57 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
 import throttle from 'lodash.throttle';
+
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+populateForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+
+  localStorage.removeItem('feedback-form-state');
+}
+
+function populateForm() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  const parsedData = JSON.parse(savedData);
+
+  if (parsedData.email) {
+    refs.form.email.value = parsedData.email;
+  }
+
+  if (parsedData.message) {
+    refs.form.message.value = parsedData.message;
+  }
+}
